4e28efff9403b71bf40267e7b477ab670671a72f
daisyshim <87219055+daisyshim@users.noreply.github.com>
2025-03-10 19:36:56 +0900

    Add files via upload

---
+++ Daisy/day7/6068_41537667(AC).py
@@ -0,0 +1,16 @@
+﻿a = int(input())
+#  90 ~ 100 : A
+#  70 ~   89 : B
+#  40 ~   69 : C
+#   0 ~   39 : D
+
+if a>=90:
+    print('A')
+else:
+    if a>=70:
+        print('B')
+    else:
+        if a>=40:
+            print('C')
+        else:
+            print('D')
