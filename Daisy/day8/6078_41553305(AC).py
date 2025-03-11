25fe78f878d081e1daed7a434f7f30c140783fb4
daisyshim <87219055+daisyshim@users.noreply.github.com>
2025-03-11 17:11:57 +0900

    Add files via upload

---
+++ Daisy/day8/6078_41553305(AC).py
@@ -0,0 +1,9 @@
+﻿n = 1 #초기값 설정
+
+while n != 0:
+    alpha = input()
+    if alpha != 'q':
+        print(alpha)
+    else:
+        print(alpha)
+        break
---
+++ Daisy/day8/6079_41562974(AC).py
@@ -0,0 +1,10 @@
+﻿a = int(input())
+
+tot = 0
+tmp = 1
+
+while tot < a :
+    tot = tot + tmp
+    tmp = tmp + 1
+    
+print(tmp-1)
